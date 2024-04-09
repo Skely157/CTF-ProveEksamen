a77a384fafa6aed25148830527a14f460ab0d717
Skely157 <131863047+Skely157@users.noreply.github.com>
2024-04-09 09:52:58 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    import Navbar from "./navbar.svelte";
+
+    function check_kyoto() {
+        const kyoto_flag = "jj-ctf{hanafuda_kort}"
+
+        if (kyoto_flag === kyoto){
+            alert("Korrekt")
+        }
+        else{
+            alert("Feil kode")
+        }
+    }
+</script>
+
+<div id="main">
+    <Navbar/>
+
+    <h1>Japan Julian-CTF</h1>
+    <br>
+
+    <img class="img" src="/Users/juela002/Documents/GitHub/CTF-ProveEksamen/src/routes/images/kyoto-header-pciture.jpeg" alt="Pent Bildet :))">
+    <br>
+
+    <p id="story">Julian sitter fast i Japan, og trenger din hjelp til å finne hans tre eiendeler for å komme hjem. <br>
+        Han husker bare at de ligger i tre byer, Kyoto, Tokyo og Hiroshima.
+    </p>
+
+    <form onsubmit=check_kyoto()>
+        <input type="text" id="kyoto" name="kyoto" placeholder="Kyoto eiendel"><br>
+        <input type="submit" value="Submit">
+    </form>
+    <br>
+    <form>
+        <input type="text" id="tokyo" name="tokyo" placeholder="Tokyo eiendel"><br>
+        <input type="submit" value="Submit">
+    </form>
+    <br>
+    <form>
+        <input type="text" id="Hiroshima" name="Hiroshima" placeholder="Hiroshima eiendel"><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
+
+<style>
+    .img{
+        width: 100px;
+        height: 100px;
+    }
+</style>
---
+++ src/routes/kyoto/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    console.log("amotY3Rme2hhbmFmdWRhX2tvcnR9=")
+</script>
+
+<h3>Ingenting å finne her.... ;)</h3>
+
+<p>Pssssttt..... sjekk console.log</p>
+
+<style>
+    p{
+        color: white;
+    }
+</style>
